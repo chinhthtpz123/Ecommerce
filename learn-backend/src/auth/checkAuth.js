7eec479b35963b7eb01d058da40e8f2ba62fff39
Vũ Xuân Chính <chinh.vuxuan852004@hcmut.edu.vn>
2025-06-19 14:47:41 +0700

    add apiKey model and check apikey permission

---
+++ learn-backend/src/auth/checkAuth.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const { findById } = require("../services/apiKey.service");
+
+const HEADER = {
+    API_KEY: 'x-api-key',
+    AUTHORIZATION: 'authorization'
+}
+
+const checkApiKey = async (req, res, next) => {
+    try {
+        const apiKey = req.headers[HEADER.API_KEY]?.toString()
+        if(!apiKey) {
+            return res.status(403).json({
+                code: 'xxxx',
+                message: 'Forbidden: Api key is required'
+            })
+        }
+
+        // check if api key exists in db
+        const objKey = await findById(apiKey);
+        if(!objKey) {
+            return res.status(403).json({
+                code: 'xxxx',
+                message: 'Forbidden: Invalid Api key'
+            })
+        }
+
+        req.objKey = objKey;
+        next();
+    } catch (error) {
+        
+    }
+}
+
+
+const permissions = (permission) => {
+    return (req, res, next) => {
+        if(!req.objKey.permissions){
+            return res.status(403).json({
+                code: 'xxxx',
+                message: 'permission denied'
+            });
+        }
+        console.log('req.objKey.permissions::', req.objKey.permissions);
+        const validPermissions = req.objKey.permissions.includes(permission);
+        if(!validPermissions) {
+            return res.status(403).json({
+                code: 'xxxx',
+                message: 'permission denied'
+            })
+        }
+        return next();
+    }
+}
+module.exports = {
+    checkApiKey,
+    permissions
+}
---
+++ learn-backend/src/services/apiKey.service.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const apiKeyModel = require('../models/apiKey.model');
+const crypto = require('crypto');
+
+const findById = async (key) => {
+    try {
+        // const newKey = await apiKeyModel.create({key: crypto.randomBytes(64).toString('hex'), permissions: ['0000']})
+        // console.log('newKey::', newKey);
+        const objectKey = await apiKeyModel.findOne({key, status: true}).lean();
+        return objectKey ? objectKey : null;
+    } catch (error) {
+        
+    }
+}
+
+module.exports = {
+    findById
+}
